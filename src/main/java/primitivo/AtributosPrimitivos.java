444236361530c6740c2bdb22a69e3decbae12a0a
Daniel Braga <daniel.braga.ext@sodexo.com>
2017-06-07 17:46:10 -0300

    Brincando com tipos primitivos

---
+++ src/main/java/primitivo/AtributosPrimitivos.java
@@ -0,0 +1,14 @@
+package primitivo;
+
+public class AtributosPrimitivos {
+
+	public static void main(String[] args) {
+		PrimitivoVO vo = new PrimitivoVO();
+		
+		System.out.println("inteiro = " + vo.getInteiro());
+		System.out.println("boolean = " + vo.isBoleano());
+		System.out.println("String  = " + vo.getString());
+
+	}
+
+}
---
+++ src/main/java/primitivo/PrimitivoVO.java
@@ -0,0 +1,33 @@
+package primitivo;
+
+public class PrimitivoVO {
+
+	private int inteiro;
+	private boolean boleano;
+	private String string;
+
+	public int getInteiro() {
+		return inteiro;
+	}
+
+	public void setInteiro(int inteiro) {
+		this.inteiro = inteiro;
+	}
+
+	public boolean isBoleano() {
+		return boleano;
+	}
+
+	public void setBoleano(boolean boleano) {
+		this.boleano = boleano;
+	}
+
+	public String getString() {
+		return string;
+	}
+
+	public void setString(String string) {
+		this.string = string;
+	}
+
+}
